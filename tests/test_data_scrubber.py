b71594e6e0231647ee2b893371b1f43aec870c1d
ShaneFred734 <shanefredrickson4@gmail.com>
2025-03-28 08:54:35 -0500

    updated test data scrubber.

--- tests/test_data_scrubber.py
+++ tests/test_data_scrubber.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from scripts.data_scrubber import DataScrubber
 r"""
 tests/test_data_scrubber.py
 
